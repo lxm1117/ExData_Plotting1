e89bddfaf03d4ab6ab007246c6b8c7c9d71e500e
ximi <ximi@debian>
2014-06-13 13:32:17 -0400

    comments added

--- plot1.R
+++ plot1.R
@@ -1,7 +1,10 @@
+#assume in the working directory
 header<- read.table("household_power_consumption.txt", sep=";", head=T, skip=0, nrows=1)
 subdata<- read.table("household_power_consumption.txt", sep=";", head=F, skip=66637, nrows=2880, col.names=names(header))
 
 x<- subdata$Global_active_power
+
+#plot the graph
 png(filename="plot1.png", width=480, height=480, units="px")
 hist(x, main="Global Active Power", xlim=range(0:6), xlab="Global Active Power (kilowatts)", col="red")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,9 +1,11 @@
+#assume in the working directory
 header<- read.table("household_power_consumption.txt", sep=";", head=T, skip=0, nrows=1)
 subdata<- read.table("household_power_consumption.txt", sep=";", head=F, skip=66637, nrows=2880, col.names=names(header))
 
 days<-paste(subdata$Date, subdata$Time)
 days<-as.POSIXlt(days, format="%d/%m/%Y %H:%M:%S")
 
+#plot the graph
 png(filename="plot4.png", width=480, height=480, units="px")
 par(mfrow=c(2,2))
 plot(days, subdata$Global_active_power, type="l", xlab=" ", ylab="Global Active Power", main="  ")
